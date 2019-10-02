35d7f615e5c90e4069cd1bf1855adb2e1aa53a8e
BogdanovPavel <tor-3000@yandex.ru>
2019-10-02 13:11:25 +0500

    Реализуем вывод информации о пользователе

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Павел'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,9 +27,14 @@ $user_name = ''; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+            <?php if ($is_auth === 1): ?>
+                <div class="user-menu__logged">
+                    <p> <?php print($user_name); ?> </p>
+                    <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                    <a class="user-menu__logout" href="#">Выход</a>
+                </div>;
+            }
+            <?php endif ?>
         </nav>
     </div>
 </header>
